136fff7d167fcf1de2a04518256079ab4f8a07ad
joeymaese <jemaese@gmail.com>
2016-03-02 16:54:00 -0500

    updated line animation

--- version-2/js/Main.js
+++ version-2/js/Main.js
@@ -9,11 +9,16 @@
     y: 0
   };
 
+  var _rowPos = {};
+
   _instance.init = function() {
     addListeners();
     buildCanvas();
     handleResize();
+    buildRows();
     draw();
+
+    requestAnimationFrame(render);
   };
 
   function buildCanvas() {
@@ -28,38 +33,39 @@
   function addListeners() {
     window.addEventListener('resize', handleResize);
     window.addEventListener('mousemove', handleMouseMove);
+  }
 
-    // requestAnimationFrame(render);
+  function buildRows() {
+    var isOffset = false;
+    var gapWidth = 16;
+
+    for (var j = 0, rowLength = 15; j < rowLength; j++) {
+      var row = 'r' + j;
+      _rowPos[row] = (isOffset) ? (gapWidth / 2) + 0.5 : 0.5;
+
+      isOffset = !isOffset;
+    }
   }
 
   function draw() {
     var LINE_HEIGHT = _canvas.height / 15;
-    var START_X = 0.5 - _mousePos.x;
-    var mouseYPerc = _mousePos.y / window.innerHeight;
-    var xPos = 0.5;
+    var START_X = 0.5;// - _mousePos.x;
+    var xPos;
     var yPos = 0;
-    var strokeWidth = 1;
-    var gapWidth = 16;// + (mouseYPerc * 8);
+    var strokeWidth = 1.5;
+    var gapWidth = 16;
     var offsetStart = 0.5;
     var isOffset = false;
 
     _ctx.clearRect(0, 0, _canvas.width, _canvas.height);
 
-
+    var currentRow = getCurrentRow();
     for (var j = 0, rowLength = 15; j < rowLength; j++) {
+      var row = 'r' + j;
+      xPos = START_X - _rowPos[row];
+
       _ctx.strokeStyle = 'rgb(0, 0, 0)';
       _ctx.lineWidth = strokeWidth;
-
-      // determine if the current row is active
-      var isRowActive = false;
-      var topOfRow = 260 + (LINE_HEIGHT * j);
-      var bottomOfRow = topOfRow + LINE_HEIGHT;
-
-      if (_mousePos.y > topOfRow && _mousePos.y < bottomOfRow) {
-        // _ctx.strokeStyle = 'rgb(55, 70, 200)';
-        // isRowActive = true;
-        xPos = START_X;
-      }
 
       for (var i = 0, lineLength = 500; i < lineLength; i++) {
         _ctx.beginPath();
@@ -87,28 +93,55 @@
 
   }
 
-  // function render(reqIndex) {
-  //   if (_mousePos.prevX != _mousePos.x || _mousePos.prevY != _mousePos.y) {
-  //     // only update the canvas when the mouse has moved.
-  //     _mousePos.prevX = _mousePos.x;
-  //     _mousePos.prevY = _mousePos.y;
-  //     draw();
-  //   }
-  //
-  //   requestAnimationFrame(render);
-  // }
+  function render(reqIndex) {
+    // if (_mousePos.prevX != _mousePos.x || _mousePos.prevY != _mousePos.y) {
+    //   // only update the canvas when the mouse has moved.
+    //   _mousePos.prevX = _mousePos.x;
+    //   _mousePos.prevY = _mousePos.y;
+    //   draw();
+    // }
+
+    draw();
+    requestAnimationFrame(render);
+  }
 
   function handleMouseMove(e) {
-    // _mousePos.prevX = _mousePos.x;
-    // _mousePos.prevY = _mousePos.y;
+    _mousePos.prevX = _mousePos.x;
+    _mousePos.prevY = _mousePos.y;
     // _mousePos.x = e.x;
+
+    _mousePos.x = e.x;
     _mousePos.y = e.y;
 
-    TweenLite.to(_mousePos, .6, {
-      x: e.x,
-      ease: Quad.easeOut,
-      onUpdate: draw
+    var currentRow = getCurrentRow();
+
+    // TweenLite.to(_mousePos, .6, {
+    //   x: e.x,
+    //   ease: Quad.easeOut,
+    //   onUpdate: draw
+    // });
+
+    TweenLite.to(_rowPos, 1, {
+      ['r' + currentRow]: _mousePos.x,
+      ease: Quad.easeOut
     });
+  }
+
+  function getCurrentRow() {
+    var LINE_HEIGHT = _canvas.height / 15;
+
+    for (var j = 0, rowLength = 15; j < rowLength; j++) {
+      // determine if the current row is active
+      var isRowActive = false;
+      var topOfRow = 260 + (LINE_HEIGHT * j);
+      var bottomOfRow = topOfRow + LINE_HEIGHT;
+
+      if (_mousePos.y > topOfRow && _mousePos.y < bottomOfRow) {
+        return j;
+      }
+    }
+
+    return -1;
   }
 
   function handleResize(e) {
